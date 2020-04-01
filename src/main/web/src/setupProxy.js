5c6e036f290dc7895c4e0bb97391d07e31324bdf
Dan Siviter <dansiviter@gmail.com>
2020-04-01 08:52:57 +0100

    Upgrading packages

--- src/main/web/src/setupProxy.js
+++ src/main/web/src/setupProxy.js
@@ -1,5 +1,5 @@
-const proxy = require('http-proxy-middleware');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
-  app.use(proxy('/websocket', { target: 'http://localhost:7001/', ws: true }));
+  app.use(createProxyMiddleware('/websocket', { target: 'http://localhost:7001/', ws: true }));
 };
